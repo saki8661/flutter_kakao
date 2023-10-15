e915470963718130da367ea18dc5059297fa1a56
saki8661 <saki8661@naver.com>
2023-10-15 14:50:41 +0900

    채팅 더미 데이터, ChatCard위젯, ChatScreen 완성

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao_app/components/chat_card.dart';
+import 'package:flutter_kakao_app/model/chat.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("ChatScreen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          "채팅",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: ListView(
+        children: List.generate(
+          chats.length,
+          (index) => ChatCard(chat: chats[index]),
+        ),
+      ),
     );
   }
 }
